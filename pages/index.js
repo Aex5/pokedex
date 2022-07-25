2dbe3e972a8a48ef6f26dd7d3509010a7af9c5b2
Sujaruu <ytfuga02@gmail.com>
2022-07-25 17:29:19 +0700

    fixing bug

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,50 @@
+import { useState } from "react";
+
+export default function index(props) {
+  const [page, setPage] = useState(9);
+  const [dataPokemon, setDataPokemon] = useState(props.initPokemon);
+
+  async function getPokemon() {
+    const req = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${page}`);
+    const { results } = await req.json();
+
+    const pokemon = results.map((poke, index) => {
+      const paddedIndex = `00${index + 1}`.slice(-3);
+      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedIndex}.png`;
+
+      return { ...poke, image };
+    });
+
+    setDataPokemon(pokemon);
+    setPage(page + 9);
+  }
+  return (
+    <>
+      <div className="w-full bg-slate-50 flex flex-col items-center">
+        <h1>Pokemon List</h1>
+        <div className="w-[45rem] grid grid-cols-3 place-items-center">
+          {dataPokemon.map((p, index) => {
+            return (
+              <div key={index}>
+                <img src={p.image} alt={p.name} />
+                <p>{`00${index + 1}`.slice(-3)}</p>
+                <p>{p.name}</p>
+              </div>
+            );
+          })}
+        </div>
+        <button onClick={getPokemon}>get pokemon</button>
+      </div>
+    </>
+  );
+}
+
+// get initial data pokemon from api
 export async function getStaticProps() {
-  const req = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+  const req = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=9`);
   const { results } = await req.json();
-  const pokemon = results.map((poke, index) => {
+
+  const initPokemon = results.map((poke, index) => {
     const paddedIndex = `00${index + 1}`.slice(-3);
     const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedIndex}.png`;
 
@@ -10,27 +53,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      pokemon,
+      initPokemon,
     },
   };
-}
-
-export default function index(props) {
-  return (
-    <>
-      <div className="w-full bg-slate-50 flex flex-col items-center">
-        <h1>Pokemon List</h1>
-        <div className="w-[45rem] grid grid-cols-3 place-items-center">
-          {props.pokemon.map((p, index) => {
-            return (
-              <div key={index}>
-                <img src={p.image} alt={p.name} />
-                <p>{p.name}</p>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </>
-  );
 }
